ec6ac24ba96c271436622c102349d9f6d41d8f06
baller4 <tkdbaker@yahoo.com>
2017-05-04 18:14:09 -0400

    Add files via upload

---
+++ Etch_A_Sketch_master.ino
@@ -0,0 +1,216 @@
+#include <TFT.h> //for LCD library
+#include <SPI.h> //for Radio
+#include <RF24.h> //for Radio
+
+//LCD Screen pins
+#define cs   10
+#define dc   9
+#define rst  8
+
+//radio pins
+#define CE_RADIO  5
+#define CSN_RADIO 6
+
+TFT TFTscreen = TFT(cs, dc, rst); //initialize the LCD screen
+RF24 radio(CE_RADIO, CSN_RADIO); //initialize the Radio
+
+const byte masterAddress[5] = {'T','X','a','a','a'};
+const byte slaveAddress[5] = {'R','x','A','A','A'};
+
+char instructions[32]; //char array to send car instructions
+int index = 0; //to keep track of instructions index
+char drawingLength = 0; //number of instructions arrays sent
+int maxImageLength = 30; //maxImageLength must match that of the slave
+
+char doneSignal[5];
+
+// initial position of the cursor
+int xPos = TFTscreen.width() / 2;
+int yPos = TFTscreen.height() / 2;
+
+int xRelative;
+int yRelative;
+
+int erasePin = 2;
+int sendPin = 4;
+
+char execute = 'A'; //execute signal
+char erase = 'B'; //erase signal
+
+boolean drawing = true;
+boolean done = false;
+
+void setup() {
+  Serial.begin(9600);
+  
+  //declare the erase and send pins as inputs to the arduino
+  pinMode(erasePin, INPUT);
+  pinMode(sendPin, INPUT);
+  
+  //setup the screen
+  TFTscreen.begin();
+  //make the background black
+  TFTscreen.background(0, 0, 0); //background(blue, green, red)
+
+  Serial.println("SimpleTx Starting");
+
+  //RADIO SETUP
+  radio.begin();
+  radio.setDataRate(RF24_250KBPS);
+  radio.setChannel(107);
+  radio.setPALevel(RF24_PA_MIN);
+  
+  radio.openWritingPipe(slaveAddress);
+  radio.openReadingPipe(1, masterAddress);
+}
+
+void loop() {
+  
+  while(drawing) {
+    draw();
+  }
+  
+  TFTscreen.background(0, 0, 255); //red to tell the user that the car is executing
+  while(!done) {
+    readData();
+  }
+
+  done = false;
+  TFTscreen.background(0, 255, 0); //green to tell the user ready to draw again
+  delay(2000);
+  
+  TFTscreen.background(0, 0, 0); //black
+  drawing = true;
+}
+
+void draw() {
+  //read the potentiometers on A0 and A1
+  int xValue = analogRead(A0);
+  int yValue = analogRead(A1);
+
+  //map the read values to incremental values
+  int mappedX = map(xValue, 0, 1023, 2, -2);
+  int mappedY = map(yValue, 0, 1023, -2, 2);
+  
+  //update the x and y positions
+  xPos = xPos + mappedX;
+  yPos = yPos + mappedY;
+
+  xRelative = mappedX * -1; //x increment relative to user
+  yRelative = mappedY;
+
+  //data only is saved if one or the other is not 0
+  if((xRelative != 0) || (yRelative != 0)) {
+    index = index + 2;
+    instructions[index-2] = xRelative;
+    instructions[index-1] = yRelative;
+  }
+  if(index == 32) { //when one packet is filled
+      send(); //send the packet
+      drawingLength = drawingLength + 1; //increase the drawing length
+      index = 0; //reset the packet
+  }
+
+  //set the screen's boarders
+  if (xPos > 159) {
+    (xPos = 159);
+  }
+  if (xPos < 0) {
+    (xPos = 0);
+  }
+  if (yPos > 127) {
+    (yPos = 127);
+  }
+  if (yPos < 0) {
+    (yPos = 0);
+  }
+
+  //draw the point
+  TFTscreen.stroke(255, 255, 255); //(255, 255, 255) is white
+  TFTscreen.point(xPos, yPos);
+
+  //if the erasePin is pressed
+  if(digitalRead(erasePin) == HIGH) {
+    TFTscreen.background(0, 0, 0);
+    index = 0;
+    drawing = false;
+    drawingLength = 0;
+    sendChar(erase); //send erase signal
+    reInitPos();
+  }
+  //if the send pin is pressed or the maxImageLength is reached
+  if ((digitalRead(sendPin) == HIGH)||(drawingLength == maxImageLength)) {
+    TFTscreen.background(0, 0, 0);
+    index = 0;
+    drawing = false;
+    sendChar(execute); //send execute signal
+    drawingLength = 0;
+    reInitPos();
+  }
+
+  delay(33);
+}
+
+//resets x and y's postion to the middle of the screen
+void reInitPos() {
+  xPos = TFTscreen.width() / 2;
+  yPos = TFTscreen.height() / 2;
+}
+
+void send() {
+  
+  radio.stopListening();
+  
+  bool rslt;
+  rslt = radio.write(&instructions, sizeof(instructions));
+      // Always use sizeof() as it gives the size as the number of bytes.
+      // For example if dataToSend was an int sizeof() would correctly return 2
+  radio.startListening();
+
+  //for debugging
+  Serial.print("Data Sent ");
+  Serial.print(instructions);
+  if (rslt) {
+    Serial.println("  Acknowledge received");
+  }
+  else {
+    Serial.println("  Tx failed");
+  }
+}
+
+void sendChar(char c) {
+  char cArr[] = {c};
+  radio.stopListening();
+  bool rslt;
+  rslt = radio.write(&cArr, sizeof(cArr));
+  radio.startListening();
+
+  //for debugging
+  Serial.print("Data Sent ");
+  Serial.print(cArr);
+  if (rslt) {
+    Serial.println("  Acknowledge received");
+  }
+  else {
+    Serial.println("  Tx failed");
+  }
+}
+
+void readData() {
+  if (radio.available()) {
+    
+    //read the data and store it in doneSignal
+    radio.read(&doneSignal, sizeof(doneSignal));
+
+    //show the read data
+    showData();
+  }
+}
+
+//mainly used for debugging
+void showData() {
+  Serial.print("Data received ");
+  done = true;
+  Serial.print(doneSignal);
+  Serial.println();
+}
---
+++ carCode.ino
@@ -0,0 +1,170 @@
+#include <SPI.h> //for Radio
+#include <RF24.h> //for Radio
+#include <MotorDriver.h> //for RC Car
+
+//radio pins
+#define CE_PIN  9
+#define CSN_PIN 10
+
+const byte thisSlaveAddress[5] = {'R','x','A','A','A'}; //address of the car
+const byte masterAddress[5] = {'T','X','a','a','a'}; //address of the Etch A Sketch
+
+RF24 radio(CE_PIN, CSN_PIN); //initialize the radio
+MotorDriver motor; //initialize the motors
+
+char carData[32]; //array that will hold incoming data
+int maxImageLength = 30; //30 packets max, this number can increase or decrease,
+                      //but limits the size of the image that can be recieved.
+char carDirections[30][32]; //must be carDirections[maxImageLength][32]
+int index = 0; //which array is being copied to
+
+char doneSignal[5] = "DONE";
+boolean done = false;
+
+void setup() {
+
+  //Begin Serial Communication for Debugging purposes
+  Serial.begin(9600);
+  Serial.println("SimpleRx Starting");
+
+  //set up for RC Car motors
+  motor.begin();
+
+  //set up for RF transiever
+  radio.begin();
+  radio.setDataRate(RF24_250KBPS);
+  radio.setChannel(107);
+  radio.setPALevel(RF24_PA_MIN);
+    
+  radio.openWritingPipe(masterAddress);
+  radio.openReadingPipe(1, thisSlaveAddress);
+  radio.startListening();
+}
+
+void loop() {
+  //while the Etch A Sketch (the master) is sending image data
+  while(!done) {
+    readData(); //read the data
+  }
+  delay(5000);
+  send(); //send the done signal
+  done = false; //reset for another image
+}
+
+void readData() {
+  if (radio.available()) {
+    
+    //read the data and store it in carData
+    radio.read(&carData, sizeof(carData));
+    
+    //show the data
+    showData();
+
+    //copy the read data into the 2D Array
+    memcpy(carDirections[index], carData, 32);
+    index = index + 1;
+    
+    if(carData[0] == 'B') { //'B' is the erase/restart signal from the master
+      Serial.println("ERASE");
+      index = 0; //"erase"
+      done = true; //done with erasing
+    }
+    if(carData[0] == 'A') { //'A' is the execute signal from the master
+      for(int i = 0; i < index-1; i++) { //for each array except the last which only contains image length
+       for(int j = 2; j < 32; j = j + 2) {
+         moveCar(carDirections[i][j-2], carDirections[i][j-1]);
+         delay(200);
+       }
+      }
+      stopCar();
+      index = 0;
+      done = true; //done without execution
+    }
+    if(index == maxImageLength) { //if the maxImageLenth is reached
+      for(int i = 0; i < index; i++) {
+       for(int j = 2; j < 32; j = j + 2) {
+         moveCar(carDirections[i][j-2], carDirections[i][j-1]);
+         delay(200);
+       }
+      }
+      stopCar();
+      index = 0;
+      done = true; //done without execution
+    }
+
+  }
+}
+
+//showData() is mainly used for debugging
+void showData() {
+  Serial.print("Data received ");
+  for(int i = 0; i < 32; i++) {
+    Serial.print(carData[i], DEC);
+  }
+  Serial.println();
+}
+
+void send() {
+  
+  radio.stopListening();
+  bool rslt;
+  rslt = radio.write(&doneSignal, sizeof(doneSignal));
+      // Always use sizeof() as it gives the size as the number of bytes.
+      // For example if dataToSend was an int sizeof() would correctly return 2
+  radio.startListening();
+
+  //for debugging
+  Serial.print("Data Sent ");
+  Serial.print(doneSignal);
+  if (rslt) {
+    Serial.println("  Acknowledge received");
+  }
+  else {
+    Serial.println("  Tx failed");
+  }
+}
+
+//moveCar(int x, int y) method combines the use of verticalMove(int a), turn(int t), and stopCar()
+void moveCar(int x, int y) {
+  if((x != 0) && (y != 0)) {
+    verticalMove(y);
+    turn(x);
+  }
+  else if(x == 0 && y == 0) {
+    stopCar();
+  }
+  else if(x == 0) {
+    verticalMove(y);
+  }
+  else if(y == 0) {
+    turn(x);
+  }
+}
+
+void verticalMove(int a) {
+  a = map(a, -2, 2, -100, 100);
+  motor.speed(0, a); //move motor 0
+  motor.speed(1, a); //move motor 1
+  //Serial.println("Vertical Move");
+}
+
+void turn(int t) {
+  if(t > 0) { //right turn, moves left motor
+    t = map(t, -2, 2, 0, 100);
+    motor.brake(0);
+    motor.speed(1, t);
+  }
+  if(t < 0) { //left turn, turns right motor
+    t = t * -1; //to match turn speed
+    t = map(t, -2, 2, 0, 100);
+    motor.speed(0, t);
+    motor.brake(1);
+  }
+  //Serial.println("Turn");
+}
+
+void stopCar() {
+  motor.brake(0); //stop motor 0
+  motor.brake(1); //stop motor 1
+  //Serial.println("Stop");
+}
